4cfbf8f025d33f6c851ed59d6664124968921316
Martín Massera <martinmassera@gmail.com>
2019-07-04 23:09:36 +0100

    scanner backend

---
+++ scanner-be/test.js
@@ -0,0 +1,5 @@
+const web3 = require('web3-eth-accounts');
+const web3utils = require('web3-utils');
+const wtContracts = require('@windingtree/wt-contracts');
+const LifDepositAbi = require('@windingtree/trust-clue-lif-deposit/build/contracts/LifDeposit.json');
+const truffleContract = require('truffle-contract');
