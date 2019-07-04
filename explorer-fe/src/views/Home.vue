ffed770d4e78fd7b94d9ca8097bb04f8d78cba72
Nathan Hamley <nathan.hamley@peakwork.com>
2019-07-04 21:48:47 +0200

    Extracted search component, added helper.js

--- explorer-fe/src/views/Home.vue
+++ explorer-fe/src/views/Home.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="hello">
-    <H1>ORG.ID explorer</H1>
-    <el-row>
-      <el-col :span="20"><el-input placeholder="Enter your ORG.ID address" v-model="orgidIn"></el-input></el-col>
-      <el-col :span="4"><el-button v-on:click="getOrgJson()" type="primary" plain>Explore</el-button></el-col>
-    </el-row>
-    <h2>DEBUG: {{ orgidIn }}</h2>
-    <h2>ORG: {{ this.currentlySelectedOrg }}</h2>
+    <SearchComponent></SearchComponent>
   </div>
 </template>
 
 <script>
-import { WtJsLibs } from '@windingtree/wt-js-libs';
-import InMemoryAdapter from '@windingtree/off-chain-adapter-in-memory';
-import axios from 'axios';
+import SearchComponent from '@/components/SearchComponent.vue';
 
 export default {
   name: 'Home',
+  components: {
+    SearchComponent
+  },
   data () {
     return {
       orgidIn: '',
@@ -28,65 +23,7 @@ export default {
   },
 
   mounted() {
-    this.libs = WtJsLibs.createInstance({
-      onChainDataOptions: {
-        provider: 'https://ropsten.infura.io/v3/7714245c4ea74010879bda16618931c9',
-      },
-      offChainDataOptions: {
-        adapters: {
-          // This is how you plug-in any off-chain data adapter you want.
-          'in-memory': {
-            options: {
-              // some: options
-            },
-            create: (options) => {
-              return new InMemoryAdapter(options);
-            },
-          },
-        },
-      },
-      // This is how you configure trust clues
-      /*
-      trustClueOptions: {
-        provider: 'http://ropsten.infura.io/v3/7714245c4ea74010879bda16618931c9', // or infura or any other ETH RPC node
-        clues: {
-          'curated-list': {
-            options: {
-              address: '0x...',
-              provider: 'http://localhost:8545',
-            },
-            create: async (options) => {
-              return new window.TrustClueCuratedList(options);
-            },
-          },
-        }
-      },*/
-    });
   },
-
-  methods: {
-    getOrgJson() {
-      console.log("Button clicked");
-      console.log("Parameter is " + this.orgidIn)
-      this.loadedOrg = this.libs.getOrganization(this.orgidIn);
-      var myThis= this;
-      this.loadedOrg.orgJsonUri.then(function(value){
-        console.log("Calling from inside promise");
-        console.log(value);
-        myThis.shownOrg =  value;
-        myThis.loadJsonFromUrl(value);
-      });
-      
-      console.log("Call done");
-    },
-
-   async loadJsonFromUrl(url) {
-      var httpResponse = await axios.get(url);
-
-      console.log(JSON.stringify(httpResponse));
-      this.currentlySelectedOrg = httpResponse.data;
-    }
-  }
 }
 </script>
 
