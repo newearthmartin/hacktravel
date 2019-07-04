8640c41b74a7b3c62fb5bc4bd481eac61be58336
Nathan Hamley <nathan.hamley@peakwork.com>
2019-07-04 21:26:44 +0200

    fixed router

--- explorer-fe/src/App.vue
+++ explorer-fe/src/App.vue
@@ -1,18 +1,10 @@
 <template>
   <div id="app">
-    <Home/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Home from './views/Home.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Home
-  }
-}
 </script>
 
 <style>
--- explorer-fe/src/router.js
+++ explorer-fe/src/router.js
@@ -7,16 +7,17 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
     {
       path: '/explorer/:orgId',
       name: 'explorerDetails',
       component: ExplorerDetails
     },
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    }
   ],
 });
