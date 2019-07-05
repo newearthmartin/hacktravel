cd458dfd1194dbabacd70b48d8f873eeb611921d
Martín Massera <martinmassera@gmail.com>
2019-07-05 08:23:22 +0100

    orgs view

--- hacktravel/scanner/views.py
+++ hacktravel/scanner/views.py
@@ -1,4 +1,17 @@
+import json
 from django.http import HttpResponse
+from .models import Org
 
 def home(request):
-    return HttpResponse('hola')
+    return HttpResponse('Access list of orgs at /orgs')
+
+def orgs_view(request):
+    orgs = Org.objects.all()
+    orgs = [{
+        'org_id': org.org_id,
+        'json_url': org.json_url,
+        'json_text': org.json_text, # maybe too much!
+        'lif_balance': org.lif_balance,
+        'trust_clues': org.trust_clues,
+    } for org in orgs]
+    return HttpResponse(json.dumps(orgs))
