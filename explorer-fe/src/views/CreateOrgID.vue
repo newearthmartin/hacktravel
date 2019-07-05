52e7cd044462d3847dfb0491a0238e5dd1beafbd
Jason Moujaes <jmoujaes@gmail.com>
2019-07-05 07:04:06 -0400

    call a contract with metamask

--- explorer-fe/src/views/CreateOrgID.vue
+++ explorer-fe/src/views/CreateOrgID.vue
@@ -37,16 +37,43 @@ export default {
   methods: {
     callCreateOrg() {
       // check if metamask is installed
-      console.log("window.ethereum: ", window.ethereum);
-      console.log("window.web3: ", window.web3);
       if (!window.ethereum || !window.web3) {
         throw new Error('Metamask not found. Is it installed as an extension?');
       }
 
+      // create ethjs object
       window.ethereum.enable();
       let eth = new Eth(window.web3.currentProvider);
-      var userAddress = eth.accounts[0];
-      console.log("userAddress: ", userAddress);
+
+      // grab the user's first account in metamask
+      var accounts = eth.accounts()
+      .then( (accounts) => {
+        let userAddress = accounts[0];
+        const organizationFactoryAddress = '0x78D1548E03660093B51159De0E615ea8F6B9eaF9';
+        const orgJsonUri = 'https://github.com/newearthmartin/hacktravel/blob/master/files/airline_safe.json';  // this will be given by the user
+
+        // create contract object for organization factory
+        var contract = eth.contract(
+            OrganizationFactory.abi,
+            undefined,
+            {"from": userAddress, "gas": 3000000}
+        ).at(organizationFactoryAddress);
+
+        // create ORG ID by calling
+        // organization factory create method
+        var result = contract["create"](orgJsonUri)
+        .then( (txid) => {
+            console.log("txid: ", txid);
+        })
+        .catch( (error) => {
+            console.log("error: ", error);
+        });
+
+      })
+      .catch( (error) => {
+        console.log("error: ", error);
+      });
+
     },
 
     getOrgJson() {
